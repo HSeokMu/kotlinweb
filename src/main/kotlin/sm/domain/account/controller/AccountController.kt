17c0609cfd13f78d7e8d1e748b7136126a662375
HSeokMu <sukmu3418@naver.com>
2024-06-25 14:08:50 +0900

    ▶ AccountController 에러 수정

--- src/main/kotlin/sm/domain/account/controller/AccountController.kt
+++ src/main/kotlin/sm/domain/account/controller/AccountController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 import sm.domain.account.model.Account
 import sm.domain.account.model.AccountDTO
 import sm.domain.account.service.AccountService
+import sm.utils.ResultWrapper
 
 @RestController
 @RequestMapping("/account")
@@ -14,7 +15,7 @@ class AccountController(
     private val accountService: AccountService
 ) {
     @PostMapping("/register")
-    fun register(@RequestBody accountDTO: AccountDTO ) {
+    fun register(@RequestBody accountDTO: AccountDTO ): ResultWrapper<Unit> {
         return accountService.saveAccount(accountDTO)
     }
 }
