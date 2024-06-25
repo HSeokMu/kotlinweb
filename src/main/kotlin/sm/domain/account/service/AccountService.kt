2955c9ae922b826f94622e6ec8f9c72ba8e0ccba
HSeokMu <sukmu3418@naver.com>
2024-06-25 14:09:22 +0900

    ▶ AccountService 추가

---
+++ src/main/kotlin/sm/domain/account/service/AccountService.kt
@@ -0,0 +1,54 @@
+package sm.domain.account.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import sm.domain.account.model.Account
+import sm.domain.account.model.AccountDTO
+import sm.domain.account.repository.AccountRepository
+import sm.utils.ResultWrapper
+
+// UserDetailsService
+// PasswordEncoder
+
+@Service
+class AccountService(
+    @Autowired
+    private val accountRepository: AccountRepository,
+    private val passwordEncoder: PasswordEncoder
+) : UserDetailsService {
+
+    @Transactional
+    fun saveAccount(accountDTO: AccountDTO): ResultWrapper<Unit> {
+        // 계정 유효성 체크
+        // 1. 기가입된 계정인지
+        if(accountRepository.existsByEmail(accountDTO.email)) {
+            return ResultWrapper.fail("이미 가입된 계정입니다.")
+        }
+
+        // 2. 이메일 인증 중인 계정인지 (생각 중)
+
+
+        // 3. 로그인 중인데 계정 생성 시도할 때 (처리 방법 생각 중)
+
+        accountRepository.save(
+            Account (
+                id = accountDTO.id,
+                password = passwordEncoder.encode(accountDTO.password),
+                email = accountDTO.email
+            )
+        )
+
+        return ResultWrapper.ok()
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        // TODO("Not yet implemented")
+        return username?.let { accountRepository.findByEmail(it)?.getAuthorities() }
+            ?: throw UsernameNotFoundException("$username Can Not Found")
+    }
+}
